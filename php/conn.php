b762abff3ddbe30a5d8810847b7a9dd0fc799453
CrisGaius <cristianocris12@hotmail.com>
2023-04-29 23:12:11 -0300

    adicionando db_name

--- php/conn.php
+++ php/conn.php
@@ -2,7 +2,7 @@
 $DB_HOST = $_ENV["DB_HOST"];
 $DB_USER = $_ENV["DB_USER"];
 $DB_PASSWORD = $_ENV["DB_PASSWORD"];
-$DB_NAME = $_ENV["DB_NAME"];
+$DB_NAME = "railway";
 $DB_PORT = $_ENV["DB_PORT"];
 
 // Create connection
