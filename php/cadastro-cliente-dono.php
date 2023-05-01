64328259ec40b8c6f898b42375c4d354e7279458
joao-ar11 <jarlpereira29@gmail.com>
2023-05-01 20:15:52 -0300

    Adicionar codigo de verificacao de login

--- php/cadastro-cliente-dono.php
+++ php/cadastro-cliente-dono.php
@@ -1,5 +1,6 @@
 <?php
     include('../php/conn.php');
+    
     //Cadastro Cliente
 
     $nome = $_POST["nome"];
@@ -11,6 +12,7 @@
     $n_endereco = $_POST["n-endereco"];
     $complemento = $_POST["complemento"];
     $senha = $_POST["senha"];
+    $senha_crip = password_hash($senha, PASSWORD_DEFAULT);
     // $senhaconfirm = $_POST["confirmar-senha"];
     $funcao = 'cliente';
 
@@ -18,8 +20,7 @@
 
     if (isset($nome) && isset($cnpj) && isset($contato) && isset($endereco) && isset($email) && isset($bairro) && isset($n_endereco) && isset($complemento) && isset($senha) && isset($funcao))  {
 
-        $insere = "INSERT INTO cadastro_c (nome, cnpj, contato, endereco, email, bairro, n_endereco, complemento, senha, funcao) VALUES ('$nome', '$cnpj', '$contato', '$endereco', '$email', '$bairro', '$n_endereco', '$complemento', '$senha', '$funcao')";
-
+        $insere = "INSERT INTO cadastro_c (contato, nome, cnpj, email, senha, n_endereco, bairro, complemento, senha_confirm, funcao) VALUES ('$contato','$nome','$cnpj','$email','$senha_crip','$n_endereco','$bairro','$complemento','$senha','$funcao')";
         mysqli_query($conn, $insere) or die("Não foi possível executar a inserção");
     }
     
